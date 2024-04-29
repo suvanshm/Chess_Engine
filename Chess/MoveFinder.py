2530edd1aa6cee41780295be0be40da671bc75d8
suvanshm <suvansh@manektala.com>
2024-04-29 19:58:14 +0530

    add piece square tables for eval

--- Chess/MoveFinder.py
+++ Chess/MoveFinder.py
@@ -1,13 +1,123 @@
 import random
+import numpy as np
 
 piece_values = {
-    'P': 1,
-    'N': 3,
-    'B': 3,
-    'R': 5,
-    'Q': 9,
-    'K': 0 # or 1000, not sure
+    'P': 100,
+    'N': 320,
+    'B': 330,
+    'R': 500,
+    'Q': 900,
+    'K': 20000 # or 1000, not sure
 }
+
+pst_white_pawn = np.array([
+    np.array([0,  0,  0,  0,  0,  0,  0,  0]),
+    np.array([50, 50, 50, 50, 50, 50, 50, 50]),
+    np.array([10, 10, 20, 30, 30, 20, 10, 10]),
+    np.array([5,  5, 10, 25, 25, 10,  5,  5]),
+    np.array([0,  0,  0, 20, 20,  0,  0,  0]),
+    np.array([5, -5,-10,  0,  0,-10, -5,  5]),
+    np.array([5, 10, 10,-20,-20, 10, 10,  5]),
+    np.array([0,  0,  0,  0,  0,  0,  0,  0])
+])
+
+pst_black_pawn = np.flip(pst_white_pawn, axis=0)
+
+pst_white_knight = np.array([
+    np.array([-50,-40,-30,-30,-30,-30,-40,-50]),
+    np.array([-40,-20,  0,  0,  0,  0,-20,-40]),
+    np.array([-30,  0, 10, 15, 15, 10,  0,-30]),
+    np.array([-30,  5, 15, 20, 20, 15,  5,-30]),
+    np.array([-30,  0, 15, 20, 20, 15,  0,-30]),
+    np.array([-30,  5, 10, 15, 15, 10,  5,-30]),
+    np.array([-40,-20,  0,  5,  5,  0,-20,-40]),
+    np.array([-50,-40,-30,-30,-30,-30,-40,-50])
+])
+
+pst_black_knight = np.flip(pst_white_knight, axis=0)
+
+
+pst_white_bishop = np.array([
+    np.array([-20,-10,-10,-10,-10,-10,-10,-20]),
+    np.array([-10,  0,  0,  0,  0,  0,  0,-10]),
+    np.array([-10,  0,  5, 10, 10,  5,  0,-10]),
+    np.array([-10,  5,  5, 10, 10,  5,  5,-10]),
+    np.array([-10,  0, 10, 10, 10, 10,  0,-10]),
+    np.array([-10, 10, 10, 10, 10, 10, 10,-10]),
+    np.array([-10,  5,  0,  0,  0,  0,  5,-10]),
+    np.array([-20,-10,-10,-10,-10,-10,-10,-20])
+])
+
+pst_black_bishop = np.flip(pst_white_bishop, axis=0)
+
+
+pst_white_rook = np.array([
+    np.array([0,  0,  0,  0,  0,  0,  0,  0]),
+    np.array([5, 10, 10, 10, 10, 10, 10,  5]),
+    np.array([-5,  0,  0,  0,  0,  0,  0, -5]),
+    np.array([-5,  0,  0,  0,  0,  0,  0, -5]),
+    np.array([-5,  0,  0,  0,  0,  0,  0, -5]),
+    np.array([-5,  0,  0,  0,  0,  0,  0, -5]),
+    np.array([-5,  0,  0,  0,  0,  0,  0, -5]),
+    np.array([0,  0,  0,  5,  5,  0,  0,  0])
+])
+
+pst_black_rook = np.flip(pst_white_rook, axis=0)
+
+pst_white_queen = np.array([
+    np.array([-20,-10,-10, -5, -5,-10,-10,-20]),
+    np.array([-10,  0,  0,  0,  0,  0,  0,-10]),
+    np.array([-10,  0,  5,  5,  5,  5,  0,-10]),
+    np.array([ -5,  0,  5,  5,  5,  5,  0, -5]),
+    np.array([  0,  0,  5,  5,  5,  5,  0, -5]),
+    np.array([-10,  5,  5,  5,  5,  5,  0,-10]),
+    np.array([-10,  0,  5,  0,  0,  0,  0,-10]),
+    np.array([-20,-10,-10, -5, -5,-10,-10,-20])
+])
+
+pst_black_queen = np.flip(pst_white_queen, axis=0)
+
+pst_white_king = np.array([
+    np.array([-30,-40,-40,-50,-50,-40,-40,-30]),
+    np.array([-30,-40,-40,-50,-50,-40,-40,-30]),
+    np.array([-30,-40,-40,-50,-50,-40,-40,-30]),
+    np.array([-30,-40,-40,-50,-50,-40,-40,-30]),
+    np.array([-20,-30,-30,-40,-40,-30,-30,-20]),
+    np.array([-10,-20,-20,-20,-20,-20,-20,-10]),
+    np.array([ 20, 20,  0,  0,  0,  0, 20, 20]),
+    np.array([ 20, 30, 10,  0,  0, 10, 30, 20])
+])
+
+pst_black_king = np.flip(pst_white_king, axis=0)
+
+pst_white_king_endgame = np.array([
+    np.array([-50,-40,-30,-20,-20,-30,-40,-50]),
+    np.array([-30,-20,-10,  0,  0,-10,-20,-30]),
+    np.array([-30,-10, 20, 30, 30, 20,-10,-30]),
+    np.array([-30,-10, 30, 40, 40, 30,-10,-30]),
+    np.array([-30,-10, 30, 40, 40, 30,-10,-30]),
+    np.array([-30,-10, 20, 30, 30, 20,-10,-30]),
+    np.array([-30,-30,  0,  0,  0,  0,-30,-30]),
+    np.array([-50,-30,-30,-30,-30,-30,-30,-50])
+])
+
+pst_black_king_endgame = np.flip(pst_white_king_endgame, axis=0)
+
+position_scores = { 
+    'wP': pst_white_pawn,
+    'wN': pst_white_knight,
+    'wB': pst_white_bishop,
+    'wR': pst_white_rook,
+    'wQ': pst_white_queen,
+    'wK': pst_white_king,
+    'bP': pst_black_pawn,
+    'bN': pst_black_knight,
+    'bB': pst_black_bishop,
+    'bR': pst_black_rook,
+    'bQ': pst_black_queen,
+    'bK': pst_black_king 
+    }
+
 checkmate_score = 1000
 draw_score = 0
 num_evaluations = 0
@@ -19,7 +129,6 @@ def find_random_move(valid_moves):
 def negamax_helper(gs, valid_moves):
     global best_move, num_evaluations
     best_move = None
-    random.seed(11)
     random.shuffle(valid_moves)
     num_evaluations = 0 # counter is reset
     #negamax(gs, valid_moves, DEPTH, color= 1 if gs.white_move else -1)
@@ -42,12 +151,13 @@ def negamax_alphabeta(gs, valid_moves, depth, alpha, beta, color):
     for move in valid_moves:
         gs.make_move(move, print_move=False)
         next_moves = gs.get_valid_moves()
+        random.shuffle(next_moves)
         eval = -negamax_alphabeta(gs, next_moves, depth-1, -beta, -alpha, -color)
         if eval > max_eval:
             max_eval = eval
             if depth == DEPTH:
                 best_move = move
-                print("best move is: " + best_move.get_notation())
+                print("best move is: " + best_move.get_notation() + "  , score = " + str(max_eval/100))
         gs.undo_move()
         if max_eval > alpha:
             alpha = max_eval
@@ -138,13 +248,15 @@ def evaluate_board(gs):
 
 def score_material(gs): 
     score = 0
-    for row in gs.board: 
-        for piece in row: 
+    for row in range(len(gs.board)):  
+        for col in range(len(gs.board[row])):
+            piece = gs.board[row][col] 
             if piece != "--":
+                position_score = position_scores[piece][row][col]
                 if piece[0] == 'w':
-                    score += piece_values[piece[1]]
+                    score += piece_values[piece[1]] + position_score
                 if piece[0] == 'b':
-                    score -= piece_values[piece[1]]
+                    score -= piece_values[piece[1]] + position_score
     return score        
 
             
